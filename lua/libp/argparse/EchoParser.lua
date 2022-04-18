29a345298a11adcbb950772fbef5412e7ab7960d
Shih-Ming Wang <swang150@ucsc.edu>
2022-04-18 02:53:15 -0700

    starter

---
+++ lua/libp/argparse/EchoParser.lua
@@ -0,0 +1,13 @@
+require("libp.utils.string_extension")
+local M = require("libp.argparse.Parser"):EXTEND()
+
+function M:parse(str)
+	vim.validate({ str = { str, "string" } })
+	return self:parse_internal(str:split())
+end
+
+function M:parse_internal(args)
+	return { { self.prog, args } }
+end
+
+return M
