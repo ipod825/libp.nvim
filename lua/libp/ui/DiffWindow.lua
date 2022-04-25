d4ed4737681f50e29466af87b9818d06591e1525
Shih-Ming Wang <swang150@ucsc.edu>
2022-04-25 00:27:38 -0700

    Refine DiffWindow.
    Setting the diff options manually will break the highlight for diff
    windows sometimes. Using diffthis command directly does not have a
    problem.

--- lua/libp/ui/DiffWindow.lua
+++ lua/libp/ui/DiffWindow.lua
@@ -1,32 +1,19 @@
 local M = require("libp.ui.Window"):EXTEND()
 local log = require("libp.log")
 
-function M:init(buffer, opts)
-	opts = opts or {}
-	opts.wo = vim.tbl_extend("force", opts.wo or {}, {
-		diff = true,
-		scrollbind = true,
-		cursorbind = true,
-		wrap = vim.o.diffopt:find("followwrap") ~= nil,
-		foldmethod = "diff",
-		winhighlight = "Normal:Normal",
-	})
-	self:SUPER():init(buffer, opts)
-end
-
 function M:open(fwin_cfg)
 	local id = self:SUPER():open(fwin_cfg)
 	local ori_win = vim.api.nvim_get_current_win()
-	-- Work around on the fold not being updated when the FileBuffer opens an
-	-- existing buffer.
+
 	if id ~= ori_win then
 		local ori_eventignore = vim.o.eventignore
 		vim.o.eventignore = "all"
 		vim.api.nvim_set_current_win(id)
-		vim.cmd("normal! zX")
+		vim.cmd("diffthis")
 		vim.api.nvim_set_current_win(ori_win)
 		vim.o.eventignore = ori_eventignore
 	else
+		vim.cmd("diffthis")
 		vim.cmd("normal! zX")
 	end
 	return id
