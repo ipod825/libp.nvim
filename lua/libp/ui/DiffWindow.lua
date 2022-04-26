22053240b534e976dca92cd1f51092e59eda3ae5
Shih-Ming Wang <swang150@ucsc.edu>
2022-04-25 23:02:37 -0700

    Use Normal hl for DiffWindow background.

--- lua/libp/ui/DiffWindow.lua
+++ lua/libp/ui/DiffWindow.lua
@@ -1,10 +1,17 @@
 local M = require("libp.ui.Window"):EXTEND()
 local log = require("libp.log")
 
+function M:init(buffer, opts)
+	opts = opts or {}
+	opts.wo = vim.tbl_extend("force", opts.wo or {}, {
+		winhighlight = "Normal:Normal",
+	})
+	self:SUPER():init(buffer, opts)
+end
+
 function M:open(fwin_cfg)
 	local id = self:SUPER():open(fwin_cfg)
 	local ori_win = vim.api.nvim_get_current_win()
-
 	if id ~= ori_win then
 		local ori_eventignore = vim.o.eventignore
 		vim.o.eventignore = "all"
