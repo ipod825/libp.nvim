724f218cddcd44503616bebfef9002542796a3a5
Shih-Ming Wang <swang150@ucsc.edu>
2022-07-31 18:13:54 -0700

    Add reflection module.

---
+++ lua/libp/debug/reflection.lua
@@ -0,0 +1,12 @@
+local M = {}
+
+function M.git_project_root()
+	return vim.fn.trim(vim.fn.system("git rev-parse --show-toplevel"))
+end
+
+function M.script_dir()
+	local str = debug.getinfo(2, "S").source:sub(2)
+	return str:match("(.*)/")
+end
+
+return M
---
+++ lua/libp/debug/reflection_spec.lua
@@ -0,0 +1,13 @@
+local reflection = require("libp.debug.reflection")
+
+describe("git_project_root", function()
+	it("Returns the git root directory", function()
+		assert.are.same(vim.fn.trim(vim.fn.system("git rev-parse --show-toplevel")), reflection.git_project_root())
+	end)
+end)
+
+describe("script_dir", function()
+	it("Returns the directory containing the script", function()
+		assert.are.same(("%s/%s"):format(reflection.git_project_root(), "lua/libp/debug"), reflection.script_dir())
+	end)
+end)
