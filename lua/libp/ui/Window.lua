13a21c78d94e06689c2db4a6439d03b3ad44b3d6
Shih-Ming Wang <swang150@ucsc.edu>
2022-08-14 00:53:27 -0700

    Allow set window variable on open.

--- lua/libp/ui/Window.lua
+++ lua/libp/ui/Window.lua
@@ -5,24 +5,30 @@ function M:init(buffer, opts)
     vim.validate({
         buffer = { buffer, "t" },
         buf_id = { buffer.id, "n" },
+        w = { opts.w, "t", true },
         wo = { opts.wo, "t", true },
         focus_on_open = { opts.focus_on_open, "b", true },
     })
 
     self.focus_on_open = opts.focus_on_open
     self.buffer = buffer
+    self.w = opts.w or {}
     self.wo = opts.wo or {}
 end
 
 function M:open(fwin_cfg)
     vim.validate({ fwin_cfg = { fwin_cfg, "t" } })
     self.id = vim.api.nvim_open_win(self.buffer.id, self.focus_on_open, fwin_cfg)
+    -- Call self:close in case closed by vim.api.nvim_win_close.
     vim.api.nvim_create_autocmd("WinClosed", {
         pattern = tostring(self.id),
         callback = function()
             self:close()
         end,
     })
+    for k, v in pairs(self.w or {}) do
+        vim.api.nvim_win_set_var(self.id, k, v)
+    end
     for k, v in pairs(self.wo) do
         vim.api.nvim_win_set_option(self.id, k, v)
     end
