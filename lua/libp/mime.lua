27411745a1c2ae9ab194804802c7e6387dae054e
Shih-Ming Wang <swang150@ucsc.edu>
2022-08-14 23:13:55 -0700

    Add mime module.

---
+++ lua/libp/mime.lua
@@ -0,0 +1,14 @@
+local M = {}
+local Job = require("libp.Job")
+
+local file_bin_available = vim.fn.executable("file")
+
+function M.info(path)
+    if file_bin_available then
+        local job = Job({ cmd = ('file --mime-type "%s"'):format(path) })
+        job:start()
+        return job:wait():stdoutputstr()
+    end
+end
+
+return M
