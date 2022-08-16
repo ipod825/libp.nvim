56dcea888633b2f1313250b9fbac3c556c6a9473
Shih-Ming Wang <swang150@ucsc.edu>
2022-08-16 01:28:04 -0700

    Add path.join_array.

--- lua/libp/path.lua
+++ lua/libp/path.lua
@@ -9,6 +9,10 @@ function M.join(...)
     return res
 end
 
+function M.join_array(arr)
+    return table.concat(arr, path_sep):gsub(path_sep .. path_sep, path_sep)
+end
+
 -- stylua: ignore start
 local kAlphaNumericalLetters = { "a", "b", "c", "d", "e", "f", "g", "h", "i",
 "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y",
