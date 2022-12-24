c0beec94f0319b4f18619e9291a6c5ed529adc78
Shih-Ming Wang <swang150@ucsc.edu>
2022-12-23 19:51:41 -0800

    Temporary disable missing setup.

--- lua/libp.lua
+++ lua/libp.lua
@@ -3,7 +3,7 @@ local M = {}
 function M.setup(opts)
     opts = vim.tbl_extend("keep", opts or {}, { integration = {  }, utils = { } })
     require("libp.integration.web_devicon").setup(opts.integration.web_devicon)
-    require("libp.utils.term").setup(opts.utils.term)
+    -- require("libp.utils.term").setup(opts.utils.term)
 end
 
 return M
